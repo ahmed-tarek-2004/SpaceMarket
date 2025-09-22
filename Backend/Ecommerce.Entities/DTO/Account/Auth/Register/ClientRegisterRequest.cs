44a70e52c048bd38bf12b7b803bd9257576ba5f1
Ahmed Tarek <ahmedtarek7580@gmail.com>
2025-09-22 04:52:11 +0300

    Validators

---
+++ Backend/Ecommerce.Entities/DTO/Account/Auth/Register/ClientRegisterRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Entities.DTO.Account.Auth.Register
+{
+    public class ClientRegisterRequest
+    {
+        public string FullName { get; set; }
+        public string OrganizationName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Country { get; set; }
+    }
+}
