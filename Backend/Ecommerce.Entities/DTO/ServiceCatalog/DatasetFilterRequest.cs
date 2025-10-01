8b7557cdf5e1cc58b996303ed4f9f54fb3be9950
Donia Shaban <doniashaban723@gmail.com>
2025-10-01 21:06:48 +0300

    Added ServiceCatalog Dataset Logic + changed in adding to Cart to accomodate adding a service or a dataset.

---
+++ Backend/Ecommerce.Entities/DTO/ServiceCatalog/DatasetFilterRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Entities.DTO.ServiceCatalog
+{
+    public class DatasetFilterRequest
+    {
+        public Guid? CategoryId { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string? ProviderId { get; set; }
+        public string? Status { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Backend/Ecommerce.Entities/DTO/ServiceCatalog/DatasetFilterResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Entities.DTO.ServiceCatalog
+{
+    public class DatasetFilterResponse
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public Guid? CategoryId { get; set; }
+        public string ProviderName { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string? ThumbnailUrl { get; set; }
+    }
+}
