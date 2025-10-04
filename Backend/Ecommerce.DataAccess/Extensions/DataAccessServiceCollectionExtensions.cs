0e59dc07ace400e314f250f00bd13eff61fc2e9c
Ahmed Tarek <ahmedtarek7580@gmail.com>
2025-10-04 04:41:44 +0300

    FixDataSetValidator

--- Backend/Ecommerce.DataAccess/Extensions/DataAccessServiceCollectionExtensions.cs
+++ Backend/Ecommerce.DataAccess/Extensions/DataAccessServiceCollectionExtensions.cs
@@ -36,7 +36,7 @@ namespace Ecommerce.DataAccess.Extensions
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    configuration.GetConnectionString("ProdCS")));
+                    configuration.GetConnectionString("DevCS")));
 
             return services;
         }
